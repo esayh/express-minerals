aa07ab48ec53162556b7a74dd8bd11a10c30ebac
EsayH <ez@EsayHs-Mac-mini.local>
2021-06-08 11:24:34 -0400

    added rocks.js

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 // Dependencies
 const express = require('express')
+const rocks = require('./models/rocks.js')
 
 // Config
 const app = express()
@@ -7,6 +8,15 @@ const app = express()
 //Routes
 app.get('/', (req, res) => {
   res.send("Welcome to Express Minerals App")
+})
+
+app.get('/rocks', (req, res) => {
+  res.send(rocks)
+})
+
+app.get('/rocks/:index', (req, res) => {
+  const { index } = req.params
+  res.send(rocks[index])
 })
 
 //Export
